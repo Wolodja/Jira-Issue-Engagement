7b8d1e21b91692c8527d77b07fcb4e6d966eae44
Wolodja <wolodjawaskovych@i.ua>
2022-12-13 15:17:45 +0100

    Building issue panel

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,25 @@
-import ForgeUI, { render, Fragment, Text, IssuePanel } from '@forge/ui';
+import ForgeUI, {render, Fragment, Text, IssuePanel, useProductContext, useState} from '@forge/ui';
+import api, {route} from '@forge/api';
+
+const fetchNumberOfComments = async function (issueKey) {
+    const response = await api.asApp().requestJira(route`/rest/api/3/issue/${issueKey}/comment`);
+    const data = await response.json();
+    return data.total;
+}
 
 const EngagementPanel = () => {
-  return (
-    <Fragment>
-      <Text>Hello world!</Text>
-    </Fragment>
-  );
+    const {platformContext: {issueKey}} = useProductContext();
+
+    const [numComments] = useState(fetchNumberOfComments(issueKey));
+    return (
+        <Fragment>
+            <Text>Engagement score: {numComments}</Text>
+        </Fragment>
+    );
 };
 
 export const panel = render(
-  <IssuePanel>
-    <EngagementPanel />
-  </IssuePanel>
+    <IssuePanel>
+        <EngagementPanel/>
+    </IssuePanel>
 );
